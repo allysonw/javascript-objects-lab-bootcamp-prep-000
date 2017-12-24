c0ae9d40b1f68a6eb45bb8b3cc893c91a2715b18
Allyson Wesman <allysonwesman@gmail.com>
2017-12-24 06:24:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = {};
 
 /*returns an object with the orignal key value pairs and the new key
  value pair:*/
-updateObjectWithKeyAndValue(object, key, value)
+function updateObjectWithKeyAndValue(object, key, value)
 {
   object[key]=value;
   return object;
