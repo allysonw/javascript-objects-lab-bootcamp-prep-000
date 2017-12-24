1666c48763f6d8386951ea09758788778c0d7d9d
Allyson Wesman <allysonwesman@gmail.com>
2017-12-24 06:36:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
 ive) */
 function deleteFromObjectByKey(object, key)
 {
-  
+  var newObject = Object.assign({}, object);
+  delete newObject[key];
+  return newObject;
 }
